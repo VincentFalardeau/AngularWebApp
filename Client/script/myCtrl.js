2e303885fd73666626db2c67915698c3121376d0
VincentFalardeau <falardeau.vince@gmail.com>
2020-06-24 12:11:58 -0400

    About to perform first request from client

---
+++ Client/script/myCtrl.js
@@ -0,0 +1,17 @@
+app.controller('myCtrl', function($scope) {
+    $scope.firstName= "";
+    $scope.lastName= "Doe";
+    $scope.email= "gustav.doe@gmail.com";
+
+    $scope.firstNames = [
+        'Jani',
+        'Carl',
+        'Margareth',
+        'Hege',
+        'Joe',
+        'Gustav',
+        'Birgit',
+        'Mary',
+        'Kai'
+    ];
+});
