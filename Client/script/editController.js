baa48d73aac159787c3b3ce9be58aef0019cf249
VincentFalardeau <falardeau.vince@gmail.com>
2020-06-24 15:56:18 -0400

    basic angular app advancement

--- Client/script/editController.js
+++ Client/script/editController.js
@@ -5,6 +5,7 @@ app.controller('editController', function($scope, $http) {
         $http.get("http://127.0.0.1:8080/marks/course?id=" + $scope.course.id).then(function (response) {
 
             $scope.marks = response.data;
+            $scope.selectedMark = $scope.marks[0];
 
         });
     };
@@ -17,6 +18,16 @@ app.controller('editController', function($scope, $http) {
         $scope.course = $scope.courses[0];
 
         refreshMarks();
+    });
+
+     $http.get("http://127.0.0.1:8080/categories/all").then(function (response) {
+
+        $scope.categories = response.data;
+        $scope.category = $scope.categories[0];
 
     });
+
+    $scope.edit = function(mark){
+        $scope.selectedMark = mark;
+    }
 });
